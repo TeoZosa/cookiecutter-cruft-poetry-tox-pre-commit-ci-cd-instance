9616d088f313a05915687a3b465e94a64a346330
Teofilo Zosa <TeoZosa@users.noreply.github.com>
2021-01-17 11:46:36 -0800

    Merge pull request #18 from TeoZosa/migrate-cli-to-typer
    Closes RND-864
    
    Original-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@c772bf5

--- cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance/__main__.py
+++ cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance/__main__.py
@@ -1,16 +1,32 @@
 """Command-line interface."""
-import click
+from typing import Optional
+
+import pkg_resources
 import structlog_sentry_logger
+import typer
 from dotenv import find_dotenv, load_dotenv
+
+__version__ = pkg_resources.get_distribution("cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance").version
+app = typer.Typer()
 
 # see `.env` for requisite environment variables
 load_dotenv(find_dotenv())
 LOGGER = structlog_sentry_logger.get_logger()
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
+def version_callback(value: Optional[bool]):  # pylint: disable=unsubscriptable-object
+    """Provides a version option for the CLI"""
+    if value:
+        typer.echo(f"cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance Version: {__version__}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Optional[  # pylint: disable=unsubscriptable-object,unused-argument
+        bool
+    ] = typer.Option(None, "--version", callback=version_callback, is_eager=True),
+) -> None:
     """Cookiecutter Cruft Poetry Tox Pre Commit Ci Cd Instance."""
     LOGGER.info(
         "For `structlog_sentry_logger` usage documentation, see: "
@@ -20,4 +36,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main(prog_name="cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance")  # pragma: no cover
+    app(prog_name="cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance")  # pragma: no cover
