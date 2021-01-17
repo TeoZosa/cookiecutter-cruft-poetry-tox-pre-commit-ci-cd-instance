8549dc6ddc8b3462ae248b82359be56e05595f20
Teo Zosa <teofilo@sonosim.com>
2021-01-16 17:19:46 -0800

    :alembic: Test Typer migration
    Original-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@6f30b04

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,12 @@
 """Test cases for the __main__ module."""
-import pytest
-from click.testing import CliRunner
+from typer.testing import CliRunner
 
-from cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance import __main__
+# from cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance import __main__
 
-
-@pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
+runner = CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
+def test_main_succeeds() -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
