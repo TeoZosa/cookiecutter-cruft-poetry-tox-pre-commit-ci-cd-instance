81dc7f32614dd0c8499bf21a66e64fb5de30ba89
github-action-benchmark <github@users.noreply.github.com>
2021-02-01 18:21:07 +0000

    add Benchmark (pytest) benchmark result for 920d21140c781698580f0d4fa5814386d1792ba1

---
+++ dev/py37-benchmark/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1612203667603,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "920d21140c781698580f0d4fa5814386d1792ba1",
+          "message": ":memo: Add link to local `icontract` benchmarks\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@f4a97d4",
+          "timestamp": "2021-02-01T18:16:28Z",
+          "tree_id": "81984a4d20b329926b9090248c08155012eacdf0",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/920d21140c781698580f0d4fa5814386d1792ba1"
+        },
+        "date": 1612203665809,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 948.6945761252813,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028912300992943614",
+            "extra": "mean: 1.0540800223442446 msec\nrounds: 537"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1276.4573784171898,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016612739911805712",
+            "extra": "mean: 783.4182456134981 usec\nrounds: 1254"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8885.775328494203,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004996355844498671",
+            "extra": "mean: 112.53941980653944 usec\nrounds: 1858"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "920d21140c781698580f0d4fa5814386d1792ba1",
+          "message": ":memo: Add link to local `icontract` benchmarks\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@f4a97d4",
+          "timestamp": "2021-02-01T18:16:28Z",
+          "tree_id": "81984a4d20b329926b9090248c08155012eacdf0",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/920d21140c781698580f0d4fa5814386d1792ba1"
+        },
+        "date": 1612203665809,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 948.6945761252813,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028912300992943614",
+            "extra": "mean: 1.0540800223442446 msec\nrounds: 537"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1276.4573784171898,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016612739911805712",
+            "extra": "mean: 783.4182456134981 usec\nrounds: 1254"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8885.775328494203,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004996355844498671",
+            "extra": "mean: 112.53941980653944 usec\nrounds: 1858"
+          }
+        ]
+      }
+    ]
+  }
+}
