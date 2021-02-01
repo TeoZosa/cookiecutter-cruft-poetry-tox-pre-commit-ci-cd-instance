1d88686f737cc7465cb0cea2f5f692773cebd275
github-action-benchmark <github@users.noreply.github.com>
2021-02-01 18:30:34 +0000

    add Benchmark (pytest) benchmark result for 9b51f1ca43f7d3673c355abf01685856494bead2

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612204091146,
+  "lastUpdate": 1612204234028,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000006672706108253772",
             "extra": "mean: 81.03268263842698 usec\nrounds: 1774"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "9b51f1ca43f7d3673c355abf01685856494bead2",
+          "message": ":memo: Update `icontract` benchmark links\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@082d94c",
+          "timestamp": "2021-02-01T18:26:09Z",
+          "tree_id": "3d3fa1a6fb3c0ddef851f0b5db34c0cbe2d313fa",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/9b51f1ca43f7d3673c355abf01685856494bead2"
+        },
+        "date": 1612204233002,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1118.0975247611586,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000033033519133984776",
+            "extra": "mean: 894.3763650792574 usec\nrounds: 567"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1636.4354263435143,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000021666366761585355",
+            "extra": "mean: 611.0843018318303 usec\nrounds: 1365"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 12505.97947321916,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007194086480654717",
+            "extra": "mean: 79.96174966874388 usec\nrounds: 1506"
           }
         ]
       }
