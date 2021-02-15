e5e9e034d70d00e0ab5240ad857eccb76485670c
github-action-benchmark <github@users.noreply.github.com>
2021-02-15 14:18:17 +0000

    add Benchmark (pytest) benchmark result for d480f865111ac9b525935525c3e4a5ed12a89989

---
+++ dev/py37-benchmark/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1613398697231,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "d480f865111ac9b525935525c3e4a5ed12a89989",
+          "message": "Merge pull request #72 from TeoZosa/dependabot/pip/myst-parser-0.13.5\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@5e0e385",
+          "timestamp": "2021-02-15T14:13:36Z",
+          "tree_id": "a24b82dba57e6052dcb41f85fe9c0eecbc96a6a9",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/d480f865111ac9b525935525c3e4a5ed12a89989"
+        },
+        "date": 1613398693310,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 918.757333024453,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004808327363203023",
+            "extra": "mean: 1.0884266868467918 msec\nrounds: 479"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1236.0744436185657,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002117227740801768",
+            "extra": "mean: 809.0127622674037 usec\nrounds: 1182"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8758.041566126176,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000059131498778583363",
+            "extra": "mean: 114.18077802550512 usec\nrounds: 1784"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "d480f865111ac9b525935525c3e4a5ed12a89989",
+          "message": "Merge pull request #72 from TeoZosa/dependabot/pip/myst-parser-0.13.5\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@5e0e385",
+          "timestamp": "2021-02-15T14:13:36Z",
+          "tree_id": "a24b82dba57e6052dcb41f85fe9c0eecbc96a6a9",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/d480f865111ac9b525935525c3e4a5ed12a89989"
+        },
+        "date": 1613398693310,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 918.757333024453,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004808327363203023",
+            "extra": "mean: 1.0884266868467918 msec\nrounds: 479"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1236.0744436185657,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002117227740801768",
+            "extra": "mean: 809.0127622674037 usec\nrounds: 1182"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8758.041566126176,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000059131498778583363",
+            "extra": "mean: 114.18077802550512 usec\nrounds: 1784"
+          }
+        ]
+      }
+    ]
+  }
+}
