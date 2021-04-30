da9b115ffd627eb314b80602d76328cc3d7a5540
github-action-benchmark <github@users.noreply.github.com>
2021-04-30 19:00:54 +0000

    add Benchmark (pytest) benchmark result for 9c1f76a003d049d03720a5e43c0948dd92cfdccd

---
+++ dev/py38-benchmark/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1619809254529,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "9c1f76a003d049d03720a5e43c0948dd92cfdccd",
+          "message": "Merge pull request #156 from TeoZosa/dependabot/pip/black-21.4b2\n\n⬆️ Bump black from 21.4b0 to 21.4b2\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@14a2971",
+          "timestamp": "2021-04-30T18:56:18Z",
+          "tree_id": "ced55a97e20d118f686777fba97c50d54b759836",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/9c1f76a003d049d03720a5e43c0948dd92cfdccd"
+        },
+        "date": 1619809251413,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 938.3791244337656,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028878800090390325",
+            "extra": "mean: 1.065667355508806 msec\nrounds: 481"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1306.8824730998622,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0001724365503520446",
+            "extra": "mean: 765.179746904133 usec\nrounds: 1292"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8403.674319573263,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000056665212486856514",
+            "extra": "mean: 118.99556812558387 usec\nrounds: 1600"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "9c1f76a003d049d03720a5e43c0948dd92cfdccd",
+          "message": "Merge pull request #156 from TeoZosa/dependabot/pip/black-21.4b2\n\n⬆️ Bump black from 21.4b0 to 21.4b2\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@14a2971",
+          "timestamp": "2021-04-30T18:56:18Z",
+          "tree_id": "ced55a97e20d118f686777fba97c50d54b759836",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/9c1f76a003d049d03720a5e43c0948dd92cfdccd"
+        },
+        "date": 1619809251413,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 938.3791244337656,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028878800090390325",
+            "extra": "mean: 1.065667355508806 msec\nrounds: 481"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1306.8824730998622,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0001724365503520446",
+            "extra": "mean: 765.179746904133 usec\nrounds: 1292"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8403.674319573263,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000056665212486856514",
+            "extra": "mean: 118.99556812558387 usec\nrounds: 1600"
+          }
+        ]
+      }
+    ]
+  }
+}
