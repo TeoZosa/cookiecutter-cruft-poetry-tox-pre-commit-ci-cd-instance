dae5377b31f7b75d73ee3e8efbf5097f8a14dfff
github-action-benchmark <github@users.noreply.github.com>
2021-05-19 19:46:51 +0000

    add Benchmark (pytest) benchmark result for f2ab290eaddebe3ce40f40872df8400942e9ab0b

---
+++ dev/py39-benchmark/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1621453611118,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "f2ab290eaddebe3ce40f40872df8400942e9ab0b",
+          "message": ":fire: Remove inactive Dependabot badge\n\nSee: dependabot/dependabot-core#1912\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@ed6b76c",
+          "timestamp": "2021-05-19T19:41:18Z",
+          "tree_id": "a747307a240e269c83a290ff448f0e45a92108bd",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/f2ab290eaddebe3ce40f40872df8400942e9ab0b"
+        },
+        "date": 1621453607531,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 991.0599399879823,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006114409553378132",
+            "extra": "mean: 1.0090207056619866 msec\nrounds: 530"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1366.7030821633646,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00009165322505454577",
+            "extra": "mean: 731.68782089603 usec\nrounds: 1273"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8757.514506259518,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000022128950501186357",
+            "extra": "mean: 114.187649850336 usec\nrounds: 1665"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "f2ab290eaddebe3ce40f40872df8400942e9ab0b",
+          "message": ":fire: Remove inactive Dependabot badge\n\nSee: dependabot/dependabot-core#1912\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@ed6b76c",
+          "timestamp": "2021-05-19T19:41:18Z",
+          "tree_id": "a747307a240e269c83a290ff448f0e45a92108bd",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/f2ab290eaddebe3ce40f40872df8400942e9ab0b"
+        },
+        "date": 1621453607531,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 991.0599399879823,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006114409553378132",
+            "extra": "mean: 1.0090207056619866 msec\nrounds: 530"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1366.7030821633646,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00009165322505454577",
+            "extra": "mean: 731.68782089603 usec\nrounds: 1273"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8757.514506259518,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000022128950501186357",
+            "extra": "mean: 114.187649850336 usec\nrounds: 1665"
+          }
+        ]
+      }
+    ]
+  }
+}
