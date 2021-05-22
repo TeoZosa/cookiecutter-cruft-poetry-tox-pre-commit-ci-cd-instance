ae18fc0b16c1449be4b1e848d059d49046ef90dd
github-action-benchmark <github@users.noreply.github.com>
2021-05-22 22:04:36 +0000

    add Benchmark (pytest) benchmark result for bbbd42e917009752a28cd14701a2990737c9ff06

---
+++ dev/py39-benchmark/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1621721075875,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "bbbd42e917009752a28cd14701a2990737c9ff06",
+          "message": ":art: Trim whitespace in \"verify-user-install\" job render template block\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@25be440",
+          "timestamp": "2021-05-22T21:57:45Z",
+          "tree_id": "2765b2bdea8d6b67fe8710e25a08896d047b455f",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/bbbd42e917009752a28cd14701a2990737c9ff06"
+        },
+        "date": 1621721069500,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1068.3676726600822,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000051730639961719476",
+            "extra": "mean: 936.0073555109952 usec\nrounds: 526"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1612.9907390916987,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004956713474997294",
+            "extra": "mean: 619.9663617183048 usec\nrounds: 1280"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 12317.08108815797,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000016835332932829154",
+            "extra": "mean: 81.18806662411532 usec\nrounds: 1621"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "bbbd42e917009752a28cd14701a2990737c9ff06",
+          "message": ":art: Trim whitespace in \"verify-user-install\" job render template block\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@25be440",
+          "timestamp": "2021-05-22T21:57:45Z",
+          "tree_id": "2765b2bdea8d6b67fe8710e25a08896d047b455f",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/bbbd42e917009752a28cd14701a2990737c9ff06"
+        },
+        "date": 1621721069500,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1068.3676726600822,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000051730639961719476",
+            "extra": "mean: 936.0073555109952 usec\nrounds: 526"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1612.9907390916987,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004956713474997294",
+            "extra": "mean: 619.9663617183048 usec\nrounds: 1280"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 12317.08108815797,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000016835332932829154",
+            "extra": "mean: 81.18806662411532 usec\nrounds: 1621"
+          }
+        ]
+      }
+    ]
+  }
+}
