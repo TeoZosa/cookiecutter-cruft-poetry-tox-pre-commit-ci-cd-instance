7d936c8a97a46de8cbe38da5416bc068f333d6aa
Teofilo Zosa <TeoZosa@users.noreply.github.com>
2021-05-22 16:51:38 -0700

    Merge pull request #176 from TeoZosa/add-sentry-exception-monitoring-component
    ✨ Add Sentry Exception Monitoring Support
    
    Original-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@1f44513

--- cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance/__init__.py
+++ cookiecutter_cruft_poetry_tox_pre_commit_ci_cd_instance/__init__.py
@@ -1,4 +1,11 @@
 """Cookiecutter Cruft Poetry Tox Pre Commit Ci Cd Instance."""
 from typing import List
 
+import sentry_sdk
+from dotenv import find_dotenv, load_dotenv
+
 __all__: List[str] = []
+
+load_dotenv(find_dotenv())
+# Note: if DSN isn't defined, will silently not transmit telemetry
+sentry_sdk.init()  # pylint: disable=abstract-class-instantiated
