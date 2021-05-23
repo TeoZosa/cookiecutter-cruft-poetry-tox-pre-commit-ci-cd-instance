2510bf8c2047b18a6f342672242dca4f34fb0942
github-action-benchmark <github@users.noreply.github.com>
2021-05-23 00:08:10 +0000

    add Benchmark (pytest) benchmark result for 7d936c8a97a46de8cbe38da5416bc068f333d6aa

--- dev/py37-benchmark/data.js
+++ dev/py37-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1621727983462,
+  "lastUpdate": 1621728490273,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -45,6 +45,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00003877842741270994",
             "extra": "mean: 115.65518885636503 usec\nrounds: 1382"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "7d936c8a97a46de8cbe38da5416bc068f333d6aa",
+          "message": "Merge pull request #176 from TeoZosa/add-sentry-exception-monitoring-component\n\n✨ Add Sentry Exception Monitoring Support\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@1f44513",
+          "timestamp": "2021-05-22T23:52:25Z",
+          "tree_id": "1b79d033f8ff7319abc0578589c90029f5058bbf",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/7d936c8a97a46de8cbe38da5416bc068f333d6aa"
+        },
+        "date": 1621728489010,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1054.1455367903188,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000025034541849673",
+            "extra": "mean: 948.6356153864843 usec\nrounds: 546"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1523.3759042074275,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000028385483431078043",
+            "extra": "mean: 656.43679753506 usec\nrounds: 1299"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 11620.055590204262,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000008082332489939693",
+            "extra": "mean: 86.05810809055016 usec\nrounds: 1656"
           }
         ]
       }
