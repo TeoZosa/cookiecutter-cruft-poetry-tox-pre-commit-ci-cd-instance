1c6345dd4e62c36b418dab66f5bd8532bf375425
github-action-benchmark <github@users.noreply.github.com>
2021-06-07 22:04:30 +0000

    add Benchmark (pytest) benchmark result for 2aa0e4fb5e7d92b9fc8e5e06b03dd71a12a5d220

---
+++ dev/py38-benchmark/data.js
@@ -0,0 +1,90 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1623103470688,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "2aa0e4fb5e7d92b9fc8e5e06b03dd71a12a5d220",
+          "message": "Merge pull request #204 from TeoZosa/update-feature-documentation-formatting\n\n📝 Update Documentation Feature Taxonomy\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@965fb99",
+          "timestamp": "2021-06-07T21:57:36Z",
+          "tree_id": "1dca163ff3059df86acd9df70ce8e2a1086097ac"
+        },
+        "date": 1623103466405,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 953.6447248849103,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016241554384408425",
+            "extra": "mean: 1.048608537231393 msec\nrounds: 564"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1321.197814578063,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012884044494084282",
+            "extra": "mean: 756.8889298529149 usec\nrounds: 1283"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8531.999529854307,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000032732829629417314",
+            "extra": "mean: 117.20581986683209 usec\nrounds: 1349"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "2aa0e4fb5e7d92b9fc8e5e06b03dd71a12a5d220",
+          "message": "Merge pull request #204 from TeoZosa/update-feature-documentation-formatting\n\n📝 Update Documentation Feature Taxonomy\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@965fb99",
+          "timestamp": "2021-06-07T21:57:36Z",
+          "tree_id": "1dca163ff3059df86acd9df70ce8e2a1086097ac"
+        },
+        "date": 1623103466405,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 953.6447248849103,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016241554384408425",
+            "extra": "mean: 1.048608537231393 msec\nrounds: 564"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1321.197814578063,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012884044494084282",
+            "extra": "mean: 756.8889298529149 usec\nrounds: 1283"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8531.999529854307,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000032732829629417314",
+            "extra": "mean: 117.20581986683209 usec\nrounds: 1349"
+          }
+        ]
+      }
+    ]
+  }
+}
