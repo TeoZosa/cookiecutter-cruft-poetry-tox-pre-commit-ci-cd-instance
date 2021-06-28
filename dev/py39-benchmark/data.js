ec5774d178aabb3a7fa123433982b84f70767027
github-action-benchmark <github@users.noreply.github.com>
2021-06-28 14:20:06 +0000

    add Benchmark (pytest) benchmark result for 198c54913c6049353db381f3d5284e66abaa6528

--- dev/py39-benchmark/data.js
+++ dev/py39-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1624889995461,
+  "lastUpdate": 1624890006366,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00006553568554626269",
             "extra": "mean: 117.1182765363625 usec\nrounds: 1790"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "198c54913c6049353db381f3d5284e66abaa6528",
+          "message": "Merge pull request #82 from TeoZosa/dependabot/pip/dot-github/workflows/pip-21.1.3",
+          "timestamp": "2021-06-28T13:23:57Z",
+          "tree_id": "8cdc0991925f0445893eed9975aaf167e679c77c"
+        },
+        "date": 1624889992966,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 957.7844813959215,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00015659534361587527",
+            "extra": "mean: 1.0440762190493538 msec\nrounds: 525"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1354.6337596918534,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00013697042516782433",
+            "extra": "mean: 738.2069085798333 usec\nrounds: 1247"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 9262.671996744466,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000025431134766908683",
+            "extra": "mean: 107.96020849615188 usec\nrounds: 1789"
           }
         ]
       }
