8ee384cdf155ad7c6c9b9d98326ce6e6169be3cd
github-action-benchmark <github@users.noreply.github.com>
2021-07-14 13:54:27 +0000

    add Benchmark (pytest) benchmark result for b90056dde643d5309673732dacf0287c7a47ffd6

--- dev/py37-benchmark/data.js
+++ dev/py37-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626270727880,
+  "lastUpdate": 1626270867626,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000009604599400975455",
             "extra": "mean: 79.85961050663896 usec\nrounds: 1656"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "b90056dde643d5309673732dacf0287c7a47ffd6",
+          "message": "Merge pull request #102 from TeoZosa/dependabot/pip/tox-3.24.0",
+          "timestamp": "2021-07-14T13:24:38Z",
+          "tree_id": "fadd9daae8927b240a568909cc259f18fd5c52e9"
+        },
+        "date": 1626270865492,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1060.7546893410463,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000023264051619044765",
+            "extra": "mean: 942.7250334582186 usec\nrounds: 538"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1536.7494218284155,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002320768515799891",
+            "extra": "mean: 650.7241751945518 usec\nrounds: 1290"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 11764.40023158331,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005677162492607756",
+            "extra": "mean: 85.00220838418511 usec\nrounds: 1646"
           }
         ]
       }
