63f7094e9bc06f48fa529999838901e68f4d6d51
github-action-benchmark <github@users.noreply.github.com>
2021-07-15 13:55:19 +0000

    add Benchmark (pytest) benchmark result for 5d7b384411a8ad1057ab69b3563fde32ba06de39

--- dev/py39-benchmark/data.js
+++ dev/py39-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626357314701,
+  "lastUpdate": 1626357319670,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000046171492825365324",
             "extra": "mean: 127.14925720224024 usec\nrounds: 1458"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "5d7b384411a8ad1057ab69b3563fde32ba06de39",
+          "message": "Merge pull request #105 from TeoZosa/dependabot/github_actions/crazy-max/ghaction-github-labeler-3.1.1",
+          "timestamp": "2021-07-15T13:15:52Z",
+          "tree_id": "3c91c377c7b2f1b91592038aa89f5ff5f6db2a77"
+        },
+        "date": 1626357317094,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 950.9051828355624,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00021033609941419648",
+            "extra": "mean: 1.0516295610231492 msec\nrounds: 508"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1299.5763590462686,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000281758717058153",
+            "extra": "mean: 769.4815260673707 usec\nrounds: 1266"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 9078.980218977951,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006874563574583975",
+            "extra": "mean: 110.14452899783639 usec\nrounds: 1707"
           }
         ]
       }
