8c81eba7d5bf2d03d2479110c6e8a8ddeafc46c2
github-action-benchmark <github@users.noreply.github.com>
2021-07-20 14:05:42 +0000

    add Benchmark (pytest) benchmark result for 3c89fe52b63f30d34dafa777efda0d8cf7f8db04

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626789865723,
+  "lastUpdate": 1626789942683,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000023694535039494447",
             "extra": "mean: 117.36404729628683 usec\nrounds: 1628"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "3c89fe52b63f30d34dafa777efda0d8cf7f8db04",
+          "message": "Merge pull request #117 from TeoZosa/dependabot/pip/icontract-hypothesis-1.1.6",
+          "timestamp": "2021-07-20T13:37:59Z",
+          "tree_id": "eb4752b0be6f0c1318f1639061bf685978ae11fa"
+        },
+        "date": 1626789938890,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 990.3631776035755,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016786756316996215",
+            "extra": "mean: 1.0097305944065318 msec\nrounds: 429"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1371.512791463287,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00018703042389129365",
+            "extra": "mean: 729.1218909690849 usec\nrounds: 1229"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8962.77955232277,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000027762426386690857",
+            "extra": "mean: 111.57253106162167 usec\nrounds: 1497"
           }
         ]
       }
