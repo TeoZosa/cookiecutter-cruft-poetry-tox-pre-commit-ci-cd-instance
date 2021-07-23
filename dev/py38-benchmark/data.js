df4d52146c303b5f4f859fe85d3a1e7a1cff2232
github-action-benchmark <github@users.noreply.github.com>
2021-07-23 15:39:52 +0000

    add Benchmark (pytest) benchmark result for c355605f84d46bd1ad2a4a598ba2b96dbaf99dec

---
+++ dev/py38-benchmark/data.js
@@ -0,0 +1,90 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1627054792855,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "c355605f84d46bd1ad2a4a598ba2b96dbaf99dec",
+          "message": "Merge pull request #124 from TeoZosa/dependabot/pip/nbqa-1.0.0",
+          "timestamp": "2021-07-23T15:23:57Z",
+          "tree_id": "9294d3700ae1e7a41e49c3004d7219be0cc711ea"
+        },
+        "date": 1627054789652,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1077.9389938675897,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002083086728037398",
+            "extra": "mean: 927.6962849372869 usec\nrounds: 551"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1592.1612308528058,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000019839388048431483",
+            "extra": "mean: 628.077094594479 usec\nrounds: 1332"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 11754.935375649293,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000054339278570468065",
+            "extra": "mean: 85.0706505857557 usec\nrounds: 1451"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "c355605f84d46bd1ad2a4a598ba2b96dbaf99dec",
+          "message": "Merge pull request #124 from TeoZosa/dependabot/pip/nbqa-1.0.0",
+          "timestamp": "2021-07-23T15:23:57Z",
+          "tree_id": "9294d3700ae1e7a41e49c3004d7219be0cc711ea"
+        },
+        "date": 1627054789652,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1077.9389938675897,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002083086728037398",
+            "extra": "mean: 927.6962849372869 usec\nrounds: 551"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1592.1612308528058,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000019839388048431483",
+            "extra": "mean: 628.077094594479 usec\nrounds: 1332"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 11754.935375649293,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000054339278570468065",
+            "extra": "mean: 85.0706505857557 usec\nrounds: 1451"
+          }
+        ]
+      }
+    ]
+  }
+}
