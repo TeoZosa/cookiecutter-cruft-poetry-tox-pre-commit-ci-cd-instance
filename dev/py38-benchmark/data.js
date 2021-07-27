a474677b409cc861a39e8ae95276ec1ee33548e8
github-action-benchmark <github@users.noreply.github.com>
2021-07-27 14:14:01 +0000

    add Benchmark (pytest) benchmark result for 54db2424c2de3a8ba9d7398dd26721ef50c99663

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627395227507,
+  "lastUpdate": 1627395241957,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -82,6 +82,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000029181957493825926",
             "extra": "mean: 113.71944765105796 usec\nrounds: 1490"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "54db2424c2de3a8ba9d7398dd26721ef50c99663",
+          "message": "Merge pull request #135 from TeoZosa/dependabot/pip/hypothesis-6.14.5",
+          "timestamp": "2021-07-27T13:54:08Z",
+          "tree_id": "83f2643067fa32a9ee520b4cb3e8087df26f32cd"
+        },
+        "date": 1627395237801,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1072.4301107559222,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00018138064966138708",
+            "extra": "mean: 932.4616960774548 usec\nrounds: 510"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1549.8446906037518,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00027375760974650146",
+            "extra": "mean: 645.2259417106134 usec\nrounds: 1321"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 9204.908842685983,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00016161016880475763",
+            "extra": "mean: 108.63768637910823 usec\nrounds: 2063"
           }
         ]
       }
