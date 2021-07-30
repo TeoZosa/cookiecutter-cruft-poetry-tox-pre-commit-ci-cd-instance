325656db5a84c79c18a6fc5d835640e5947a4b3e
github-action-benchmark <github@users.noreply.github.com>
2021-07-30 13:40:25 +0000

    add Benchmark (pytest) benchmark result for fa805b14504ebde3d285ac85c05f9c0744e56b3f

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1627652392417,
+  "lastUpdate": 1627652425518,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00003022191673398384",
             "extra": "mean: 114.23118490557835 usec\nrounds: 2120"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "fa805b14504ebde3d285ac85c05f9c0744e56b3f",
+          "message": "Merge pull request #142 from TeoZosa/dependabot/pip/emoji-1.4.2",
+          "timestamp": "2021-07-30T13:23:15Z",
+          "tree_id": "e2c3669bff579fbc51a92f3d2a9f7c8e12c0cc86"
+        },
+        "date": 1627652422103,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 1015.482053295244,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0001861510301555694",
+            "extra": "mean: 984.7539863013782 usec\nrounds: 511"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1428.7275837357672,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010257332407034243",
+            "extra": "mean: 699.9234923324212 usec\nrounds: 1239"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 8823.182287125439,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000612993999176484",
+            "extra": "mean: 113.33779213189035 usec\nrounds: 1703"
           }
         ]
       }
