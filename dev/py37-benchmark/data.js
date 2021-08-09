2f768fc2dd178901928a4d91bf155f18f8212e50
github-action-benchmark <github@users.noreply.github.com>
2021-08-09 16:21:36 +0000

    add Benchmark (pytest) benchmark result for 3bc6af3a6563bf0f8d2887a9846528f2224fc05d

--- dev/py37-benchmark/data.js
+++ dev/py37-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1628525606479,
+  "lastUpdate": 1628526096623,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000010707619926384953",
             "extra": "mean: 468.3566885604309 usec\nrounds: 822"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "3bc6af3a6563bf0f8d2887a9846528f2224fc05d",
+          "message": "Merge pull request #153 from TeoZosa/dependabot/pip/python-dotenv-0.19.0",
+          "timestamp": "2021-08-09T13:50:46Z",
+          "tree_id": "539b1c981373aaa8861ce115c0758b5c02be7db0"
+        },
+        "date": 1628526093769,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 249.22478268374348,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000678770112087343",
+            "extra": "mean: 4.012442058256145 msec\nrounds: 103"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 727.5698733730511,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00021474995186724786",
+            "extra": "mean: 1.3744384375949334 msec\nrounds: 681"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1454.2442308284753,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00032621128682554045",
+            "extra": "mean: 687.6424047632668 usec\nrounds: 672"
           }
         ]
       }
