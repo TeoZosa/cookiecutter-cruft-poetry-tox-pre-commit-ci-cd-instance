080d8d8c5168e371e2240a90db53bd4d00d401c0
github-action-benchmark <github@users.noreply.github.com>
2021-08-17 14:13:13 +0000

    add Benchmark (pytest) benchmark result for 07881b888615f67110ee5e892d7d8906dbf2efa6

---
+++ dev/py38-benchmark/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1629209593753,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "07881b888615f67110ee5e892d7d8906dbf2efa6",
+          "message": "Merge pull request #287 from TeoZosa/dependabot/pip/icontract-2.5.4\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@63fb803",
+          "timestamp": "2021-08-17T13:49:43Z",
+          "tree_id": "6fb9ac1e2213bc90edcc238550dbd1f67ae9937b"
+        },
+        "date": 1629209591566,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 389.5359053812971,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002368434709098349",
+            "extra": "mean: 2.5671574460412074 msec\nrounds: 139"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 1094.7125115095516,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010880452294576742",
+            "extra": "mean: 913.481840653353 usec\nrounds: 979"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 2355.057802151457,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005520697148230484",
+            "extra": "mean: 424.6180280952988 usec\nrounds: 961"
+          }
+        ]
+      }
+    ]
+  }
+}
