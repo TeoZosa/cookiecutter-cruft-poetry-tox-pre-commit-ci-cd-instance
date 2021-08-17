8cfd7c7347ca2517d1cfabf5510ef3fef1ebbd41
github-action-benchmark <github@users.noreply.github.com>
2021-08-17 14:17:34 +0000

    add Benchmark (pytest) benchmark result for fa516b23320004be83bc9c930c086371c02d7941

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629209593753,
+  "lastUpdate": 1629209854805,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00005520697148230484",
             "extra": "mean: 424.6180280952988 usec\nrounds: 961"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "fa516b23320004be83bc9c930c086371c02d7941",
+          "message": "Merge pull request #168 from TeoZosa/dependabot/pip/icontract-2.5.4",
+          "timestamp": "2021-08-17T13:40:41Z",
+          "tree_id": "e1ab35bd28467e56ab609384ff729a815aae04dd"
+        },
+        "date": 1629209852964,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 256.9154278686498,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005429765521111331",
+            "extra": "mean: 3.8923314504540327 msec\nrounds: 111"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 756.4796862627318,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012583391434535355",
+            "extra": "mean: 1.321912561777226 msec\nrounds: 518"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1571.6521489883685,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00007872935155874042",
+            "extra": "mean: 636.2731095705076 usec\nrounds: 648"
           }
         ]
       }
