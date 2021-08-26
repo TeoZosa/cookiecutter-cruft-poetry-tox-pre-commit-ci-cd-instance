cf8cf21bd57d0d0ae819260b2ca45eb6114ef214
github-action-benchmark <github@users.noreply.github.com>
2021-08-26 13:39:22 +0000

    add Benchmark (pytest) benchmark result for fbf79cfb7b5c85dde51770acc33e4dc36dde87ae

---
+++ dev/py38-benchmark/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1629985162292,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "fbf79cfb7b5c85dde51770acc33e4dc36dde87ae",
+          "message": "Merge pull request #301 from TeoZosa/dependabot/pip/importlib-metadata-4.7.0\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@2eb4b90",
+          "timestamp": "2021-08-26T13:25:59Z",
+          "tree_id": "f355935d4f258b7d2f7dd89a6ece74071d2e8a23"
+        },
+        "date": 1629985159792,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 277.2018493693327,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00047964613458194473",
+            "extra": "mean: 3.6074795398195194 msec\nrounds: 113"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 824.1453088423618,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000208037126371276",
+            "extra": "mean: 1.213378258992523 msec\nrounds: 695"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1748.937551225514,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012365480319651408",
+            "extra": "mean: 571.7757042264208 usec\nrounds: 710"
+          }
+        ]
+      }
+    ]
+  }
+}
