b725ac404c5a456eb0e504363bb0ed7f589931bd
github-action-benchmark <github@users.noreply.github.com>
2021-08-31 14:30:23 +0000

    add Benchmark (pytest) benchmark result for 85cb138fda3d140631d044d56c8f7487fb00ea8c

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1630420137744,
+  "lastUpdate": 1630420223669,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00008335755205291194",
             "extra": "mean: 587.4569734785485 usec\nrounds: 641"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub"
+          },
+          "id": "85cb138fda3d140631d044d56c8f7487fb00ea8c",
+          "message": "Merge pull request #204 from TeoZosa/dependabot/pip/hypothesis-6.17.4",
+          "timestamp": "2021-08-31T13:35:43Z",
+          "tree_id": "d34c133d1f90bf01ec0fc2afe5717c49021378ea"
+        },
+        "date": 1630420221144,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 285.7744707249081,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003707805475382918",
+            "extra": "mean: 3.4992628888905153 msec\nrounds: 126"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 850.1568463925064,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002169032595153004",
+            "extra": "mean: 1.1762535398536482 msec\nrounds: 665"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1633.8430679782734,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00022691431401266926",
+            "extra": "mean: 612.0538866914591 usec\nrounds: 759"
           }
         ]
       }
