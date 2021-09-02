9d15f815fcdba88d8904cd04184cfec3f1fa16a1
github-action-benchmark <github@users.noreply.github.com>
2021-09-02 21:23:52 +0000

    add Benchmark (pytest) benchmark result for 798fb29a8021c8ac1f436f8d0dda473da05b24d5

---
+++ dev/py37-benchmark/data.js
@@ -0,0 +1,90 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1630617832182,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "798fb29a8021c8ac1f436f8d0dda473da05b24d5",
+          "message": "Merge pull request #314 from TeoZosa/make-adr-support-optional\n\n💥 Make Architecture Decision Records Support Optional\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@239a51a",
+          "timestamp": "2021-09-02T21:15:25Z",
+          "tree_id": "b3731d5dab81b47bea145eb69be7864bda4cadc6"
+        },
+        "date": 1630617827794,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 303.2029024597003,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003620208253834624",
+            "extra": "mean: 3.298121462187894 msec\nrounds: 119"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 873.219207343046,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002211984049982971",
+            "extra": "mean: 1.1451878194969065 msec\nrounds: 759"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1816.7098788545673,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001632503576002502",
+            "extra": "mean: 550.4456224075241 usec\nrounds: 723"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "798fb29a8021c8ac1f436f8d0dda473da05b24d5",
+          "message": "Merge pull request #314 from TeoZosa/make-adr-support-optional\n\n💥 Make Architecture Decision Records Support Optional\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@239a51a",
+          "timestamp": "2021-09-02T21:15:25Z",
+          "tree_id": "b3731d5dab81b47bea145eb69be7864bda4cadc6"
+        },
+        "date": 1630617827794,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 303.2029024597003,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003620208253834624",
+            "extra": "mean: 3.298121462187894 msec\nrounds: 119"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 873.219207343046,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002211984049982971",
+            "extra": "mean: 1.1451878194969065 msec\nrounds: 759"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1816.7098788545673,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001632503576002502",
+            "extra": "mean: 550.4456224075241 usec\nrounds: 723"
+          }
+        ]
+      }
+    ]
+  }
+}
