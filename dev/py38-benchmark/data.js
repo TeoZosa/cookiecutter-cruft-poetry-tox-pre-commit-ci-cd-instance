57d34db2de08cc0bb91b4652d37bab8d8738577c
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 20:59:05 +0000

    add Benchmark (pytest) benchmark result for 5bd4a516823b5608ded5252bb9bb14a8e5f656b6

--- dev/py38-benchmark/data.js
+++ dev/py38-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633121504458,
+  "lastUpdate": 1633121945598,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -1271,6 +1271,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000016697207547325893",
             "extra": "mean: 537.0308643359526 usec\nrounds: 715"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "5bd4a516823b5608ded5252bb9bb14a8e5f656b6",
+          "message": ":bug: Preempt premature brace expansion by the shell\n\nIf multiple commands are passed via brace syntax, quoting the string\nallows direct evaluation by tox as opposed to generating multiple envs\nin the call to tox. For example,\n`poetry run tox -e py3{8,9} -- $ (POSARGS)`\nbecomes\n`poetry run tox -e py38 py39 -- $(POSARGS)`\nwhereas\n`poetry run tox -e \"py3{8,9}\" -- $(POSARGS)`\npasses \"py3{8,9}\" directly to tox.\n\nNote: to activate this behavior via the `tox-%` make target, users would\nhave to also invoke it with quotes, e.g., `make tox-\"py3{8,9}\"`.\n- If a user were to enter `make tox-py3{8,9}`, this would become\n`make tox-py38 tox-py39\"`, which, while this would still work, leads to\ndifferent semantics as separate make targets are actually invoked.\n\nsee: https://www.gnu.org/software/bash/manual/html_node/Brace-Expansion.html\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@78c3f33",
+          "timestamp": "2021-10-01T20:40:07Z",
+          "tree_id": "630c24397fb20e9fa93e6838423c5ab671e27fd3"
+        },
+        "date": 1633121941571,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 301.3170230309391,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000048069100389383957",
+            "extra": "mean: 3.318763705883688 msec\nrounds: 119"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 880.3198224963677,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000018033330130125902",
+            "extra": "mean: 1.1359507924793162 msec\nrounds: 771"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1808.8662829532307,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014940478714155211",
+            "extra": "mean: 552.832461649602 usec\nrounds: 691"
           }
         ]
       }
