f619097e9bc23f986500bf6dd270785d0f6be722
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 21:17:55 +0000

    add Benchmark (pytest) benchmark result for d7eeca1d9e67b4f46517bd1680a8ad8a1193af48

---
+++ dev/bench/Linux/CPython/3.8/64bit/pure_python/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1633123075621,
+  "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "d7eeca1d9e67b4f46517bd1680a8ad8a1193af48",
+          "message": "Merge pull request #362 from TeoZosa/streamline-and-expand-performance-testing-workflow\n\n👷‍♀️ Streamline and Expand Performance Testing\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@587e34c",
+          "timestamp": "2021-10-01T21:00:00Z",
+          "tree_id": "ea2e1a176cf76eab14a790bf919b66f4f819c49c"
+        },
+        "date": 1633123072584,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 283.95358145249065,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000508309997526719",
+            "extra": "mean: 3.5217023672839773 msec\nrounds: 324"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 835.6580908444348,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0001980484702419776",
+            "extra": "mean: 1.1966616621751334 msec\nrounds: 1039"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1619.7626026505322,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00011209289024630083",
+            "extra": "mean: 617.3744216366208 usec\nrounds: 2163"
+          }
+        ]
+      }
+    ]
+  }
+}
