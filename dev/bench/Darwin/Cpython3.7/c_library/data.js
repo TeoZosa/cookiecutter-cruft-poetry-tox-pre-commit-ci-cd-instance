c1560402d621a8e324d054beb6e23d79c7f15c57
github-action-benchmark <github@users.noreply.github.com>
2021-10-12 17:23:51 +0000

    add Benchmark (pytest) benchmark result for 7345a49401e4cac7ea42d4e2be88de5b518bf820

--- dev/bench/Darwin/Cpython3.7/c_library/data.js
+++ dev/bench/Darwin/Cpython3.7/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634058523836,
+  "lastUpdate": 1634059431224,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -902,6 +902,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00002641778698229521",
             "extra": "mean: 582.4904392764796 usec\nrounds: 1935"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "7345a49401e4cac7ea42d4e2be88de5b518bf820",
+          "message": ":cookie: :lock: :rewind: Revert security fix introduced in `867a0dd`\n\nSuperseded by 4dcf97350eaf827088f7d08d927c50f3165e38b4.\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@cda3914",
+          "timestamp": "2021-10-12T17:07:46Z",
+          "tree_id": "a64800c8dfc5c5e378e0f1ac4d4c05763d68c8a8"
+        },
+        "date": 1634059428355,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 295.8439211998579,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00013835394067314904",
+            "extra": "mean: 3.38016071428572 msec\nrounds: 336"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 849.9547199412486,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00008152140604121743",
+            "extra": "mean: 1.1765332629356102 msec\nrounds: 947"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1713.3293996527725,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000040709937132978944",
+            "extra": "mean: 583.658927584306 usec\nrounds: 1809"
           }
         ]
       }
