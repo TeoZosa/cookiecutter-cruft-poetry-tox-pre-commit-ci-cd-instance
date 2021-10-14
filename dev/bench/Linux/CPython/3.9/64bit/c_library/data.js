acf0460697b1a20c662b9457ef148a87f2d797b0
github-action-benchmark <github@users.noreply.github.com>
2021-10-14 18:10:18 +0000

    add Benchmark (pytest) benchmark result for 31b5c8495ecefe2d2c8a1d212daf12b416acdb84

--- dev/bench/Linux/CPython/3.9/64bit/c_library/data.js
+++ dev/bench/Linux/CPython/3.9/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634232597741,
+  "lastUpdate": 1634235018474,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -451,6 +451,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0002811018142837578",
             "extra": "mean: 583.744093828417 usec\nrounds: 1993"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "31b5c8495ecefe2d2c8a1d212daf12b416acdb84",
+          "message": "Merge pull request #391 from TeoZosa/c-compile-all-project-modules-by-default\n\n📦 C-Compile All Project Modules by Default\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@e555667",
+          "timestamp": "2021-10-14T18:07:44Z",
+          "tree_id": "adb0ed2ba63cad7d0fcac014b50ede4f4e853dca"
+        },
+        "date": 1634235015139,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 292.7077401310132,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009242464440313657",
+            "extra": "mean: 3.4163770303867245 msec\nrounds: 362"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 840.0475720525195,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00023604644531509926",
+            "extra": "mean: 1.190408773584885 msec\nrounds: 1060"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1728.7087329320248,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00021928334903377735",
+            "extra": "mean: 578.4664477884149 usec\nrounds: 2193"
           }
         ]
       }
