511dee31a409a1a8406a59b8e309aad8331490d6
github-action-benchmark <github@users.noreply.github.com>
2022-02-27 00:08:28 +0000

    add Benchmark (pytest) benchmark result for c2d77a83d039af4c9b9829fa81177ecbec16cd9d

--- dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
+++ dev/bench/Linux/CPython/3.7/64bit/c_library/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642432378383,
+  "lastUpdate": 1645920508169,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -4059,6 +4059,47 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00041668912516626736",
             "extra": "mean: 760.6380983606524 usec\nrounds: 1525"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erinzosa@ucla.edu",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action"
+          },
+          "id": "c2d77a83d039af4c9b9829fa81177ecbec16cd9d",
+          "message": ":heavy_minus_sign: Remove `flakeheaven` from dev deps\n\nSufficient to run it solely via `pre-commit`.\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@30559bb",
+          "timestamp": "2022-02-26T23:54:16Z",
+          "tree_id": "c82b9da1258bdbce962ee1c17d86607def9d8b54"
+        },
+        "date": 1645920504424,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 265.94307200223653,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004960412588777102",
+            "extra": "mean: 3.760203236245952 msec\nrounds: 309"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 763.2296934277074,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028392561756122826",
+            "extra": "mean: 1.3102215605749612 msec\nrounds: 974"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 1413.531830270006,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00017060324718813736",
+            "extra": "mean: 707.4478116343407 usec\nrounds: 1805"
           }
         ]
       }
