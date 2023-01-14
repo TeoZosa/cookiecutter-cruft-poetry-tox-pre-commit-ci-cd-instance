4cfdbba88cb1c466af0f62b799e1237c72c2b97c
github-action-benchmark <github@users.noreply.github.com>
2023-01-14 07:30:35 +0000

    add Benchmark (pytest) benchmark result for 7be913f0b9b301d77804680f303cf2874d7002a5

--- dev/bench/Linux/CPython/3.8/64bit/pure_python/data.js
+++ dev/bench/Linux/CPython/3.8/64bit/pure_python/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673069716515,
+  "lastUpdate": 1673681435164,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance",
   "entries": {
     "Benchmark": [
@@ -6979,6 +6979,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000021425929226764886",
             "extra": "mean: 383.70383327132055 usec\nrounds: 2687"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "TeoZosa@users.noreply.github.com",
+            "name": "Teofilo Zosa",
+            "username": "TeoZosa"
+          },
+          "committer": {
+            "email": "action@github.com",
+            "name": "GitHub Action",
+            "username": "actions-user"
+          },
+          "distinct": true,
+          "id": "7be913f0b9b301d77804680f303cf2874d7002a5",
+          "message": "Merge pull request #943 from TeoZosa/ci/unpin-nbqa-deps\n\n📌 Unpin `nbqa` deps\n\nOriginal-Commit: TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd@39b2ebd",
+          "timestamp": "2023-01-14T07:26:18Z",
+          "tree_id": "f5f830f3c44d665f1176b5afd30184b74647aaa4",
+          "url": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd-instance/commit/7be913f0b9b301d77804680f303cf2874d7002a5"
+        },
+        "date": 1673681428318,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_main.py::test_main_succeeds",
+            "value": 413.6507889630957,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000027172337414844234",
+            "extra": "mean: 2.4174981087470284 msec\nrounds: 423"
+          },
+          {
+            "name": "tests/test_main.py::test_version_option",
+            "value": 919.5810784733812,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002477894786707031",
+            "extra": "mean: 1.08745169230768 msec\nrounds: 975"
+          },
+          {
+            "name": "tests/test_main.py::test_version_callback",
+            "value": 2644.2608846538014,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007394701535303951",
+            "extra": "mean: 378.17751107827036 usec\nrounds: 2708"
           }
         ]
       }
